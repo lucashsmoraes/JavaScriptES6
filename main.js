1a12c4bce3c41f7330007a9a8b85e830c54ecb88
Lucas <lucashsmoraes@gmail.com>
2019-02-12 00:53:55 -0200

    entendendo classes com metodos static

--- main.js
+++ main.js
@@ -1,31 +1,8 @@
-class List {
-    constructor(){
-        this.data = [];
-    }
 
-    add(data){
-        this.data.push(data);
-        console.log(this.data)
+class Matematica {
+    static soma(a, b){
+        return a + b;
     }
 }
 
-
-
-class TodoList extends List{
-    constructor(){
-        super()
-        this.usuario = 'Lucas'
-    }
-
-    mostrarUsuario(){
-        console.log(this.usuario)
-    }
-}
-
-const MinhaLista = new TodoList();
-
-document.getElementById("novo-todo").onclick = function() {
-  MinhaLista.add("Novo - todo");
-};
-
-MinhaLista.mostrarUsuario()
+console.log(Matematica.soma(1,2))
