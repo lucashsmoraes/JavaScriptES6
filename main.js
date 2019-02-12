55c1a21fd3a5f31baf54b2138594ade5af7da178
Lucas <lucashsmoraes@gmail.com>
2019-02-12 02:10:12 -0200

    usando ex anterior e passando para arrow function

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
 
 const arr = [1,2,3,4,5,6]
 
-const newArr = arr.map(function(item){
-    return item * 2
-})
+const newArr = arr.map(item =>  item * 2)
 
 console.log(newArr)
 
-const sum = arr.reduce(function(total, next){
-    return total + next;
-})
+const sum = arr.reduce((total, next) => total + next)
 
 console.log(sum)
 
-const filter = arr.filter(function(item){
-    return item % 2 === 0
-})
+const filter = arr.filter(item => item % 2 === 0)
 
 console.log(filter)
 
-const find = arr.find(function(item){
-    return item === 4
-})
+const find = arr.find(item => item === 4)
 
 console.log(find)
