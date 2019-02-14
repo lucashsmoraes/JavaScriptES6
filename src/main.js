110f139cbd6d14d5a5169e8baaf8dc3568f86096
Lucas <lucashsmoraes@gmail.com>
2019-02-14 21:01:53 -0200

    usando Async/await e install (plugin-transform-async-to-generator, @babel/polyfill)

--- src/main.js
+++ src/main.js
@@ -1,24 +1,11 @@
+// Async/await
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => {resolve('Ok')}, 2000)
+})
 
-const arr = [1,2,3,4,5,6]
+async function executaPromise() {
+    const response = await minhaPromise();
+    console.log(response)
+}
 
-const newArr = arr.map(item =>  item * 2)
-
-console.log(newArr)
-
-const sum = arr.reduce((total, next) => total + next)
-
-console.log(sum)
-
-const filter = arr.filter(item => item % 2 === 0)
-
-console.log(filter)
-
-const find = arr.find(item => item === 4)
-
-console.log(find)
-
-console.log("------------------------------------------------")
-console.log("Testando Webpack")
-
-import { soma } from '../funcoes/funcoes'
-console.log(soma(1,2))
+executaPromise()
