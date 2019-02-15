06599e05ac1ec0208e2ec8c3fc7e6353b1fb4957
Lucas <lucashsmoraes@gmail.com>
2019-02-14 23:18:00 -0200

    render em tela com dados staticos

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ class App {
 		this.repositories = [];
 
 		this.formEl = document.getElementById('repo-form');
+		this.listEl = document.getElementById('repo-list')
 
 		this.registerHandlers();
 	}
@@ -21,6 +22,35 @@ class App {
 			description: 'descrição',
 			avatar_url: 'https://avatars1.githubusercontent.com/u/32342356?v=4',
 			html_url: 'https://github.com/lucashsmoraes/JavaScriptES6'
+		})
+		this.render();
+	}
+
+	render() {
+		this.listEl.innerHTML = '';
+
+		this.repositories.forEach(repo => {
+
+			let imgEl = document.createElement('img')
+			imgEl.setAttribute('src', repo.avatar_url)
+
+			let titleEl = document.createElement('strong')
+			titleEl.appendChild(document.createTextNode(repo.name))
+
+			let descriptionEl = document.createElement('p')
+			descriptionEl.appendChild(document.createTextNode(repo.description))
+
+			let linkEl = document.createElement('a')
+			linkEl.setAttribute('target', '_blank')
+			linkEl.appendChild(document.createTextNode('Acessar'))
+
+			let listItemEl = document.createElement('li')
+			listItemEl.appendChild(imgEl)
+			listItemEl.appendChild(titleEl)
+			listItemEl.appendChild(descriptionEl)
+			listItemEl.appendChild(linkEl)
+
+			this.listEl.appendChild(listItemEl)
 		})
 	}
 }
